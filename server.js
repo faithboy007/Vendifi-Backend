622123fffea74bdf06888e75847791d184503785
faithboy007 <faithboluwatife11@gmail.com>
2025-11-12 00:15:24 +0000

    pro id

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 // --- FIREBASE INITIALIZATION ---
 admin.initializeApp({
     credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_project_id,
+        projectId: process.env.FIREBASE_PROJECT_ID,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     })
